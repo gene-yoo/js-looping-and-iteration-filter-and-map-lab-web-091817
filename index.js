10ee404a73923c5761d4af6b2aeec087a24cdd57
gene-yoo <gene.y.yoo@gmail.com>
2017-10-30 14:14:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here:
+function driversWithRevenueOver (drivers, threshold) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > threshold;
+  });
+};
+
+function driverNamesWithRevenueOver (drivers, threshold) {
+  eligibleDrivers = driversWithRevenueOver(drivers, threshold)
+
+  return eligibleDrivers.map(function(driver) { return driver.name })
+};
+
+function exactMatch (drivers, attributeObj) {
+  return drivers.filter(function (driver) {
+    for (const key in attributeObj) {
+      return driver[key] === attributeObj[key];
+    };
+  });
+};
+
+function exactMatchToList (drivers, attributeObj) {
+  eligibleDrivers = exactMatch(drivers, attributeObj);
+
+  return eligibleDrivers.map(function (driver) { return driver.name })
+};
